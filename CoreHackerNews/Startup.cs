42587c3cbff8d173a692804584dced7ec70c70c0
Rui Taborda <rtaborda@obi4wan.com>
2020-12-20 18:42:19 +0000

    Add auto-generated swagger documentation.

--- CoreHackerNews/Startup.cs
+++ CoreHackerNews/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace CoreHackerNews
 {
@@ -28,6 +29,11 @@ namespace CoreHackerNews
                 .AddSingleton<IHackerNewsClient, HackerNewsClient>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CoreHackerNews API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +48,13 @@ namespace CoreHackerNews
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "api/documentation";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoreHackerNews API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
